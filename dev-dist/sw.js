d34962d3c09ba9e493a8f2100620aab42e7b38d1
仙人 <1342125823@qq.com>
2026-08-12 02:19:44 +0800

    asd

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -76,6 +76,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    * requests for URLs in the manifest.
    * See https://goo.gl/S9QRab
    */
+
   workbox.precacheAndRoute([{
     "url": "registerSW.js",
     "revision": "3ca0b8505b4bec776b69afdba2768812"
